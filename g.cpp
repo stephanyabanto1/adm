774e8d44ee7e1db2a5ff5fdb27a19e67ac53f0b0
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-02-12 18:47:38 -0500

    test

--- g.cpp
+++ g.cpp
@@ -25,9 +25,9 @@ void MPU6050_Init(){
 	
 	wiringPiI2CWriteReg8 (fd, SMPLRT_DIV, 0x07);	/* Write to sample rate register */
 	wiringPiI2CWriteReg8 (fd, PWR_MGMT_1, 0x01);	/* Write to power management register */
-	wiringPiI2CWriteReg8 (fd, CONFIG, 0);		/* Write to Configuration register */
-	wiringPiI2CWriteReg8 (fd, GYRO_CONFIG, 24);	/* Write to Gyro Configuration register */
-	wiringPiI2CWriteReg8 (fd, INT_ENABLE, 0x01);	/*Write to interrupt enable register */
+	wiringPiI2CWriteReg8 (fd, CONFIG, 0);		    /* Write to Configuration register */
+	wiringPiI2CWriteReg8 (fd, GYRO_CONFIG, 24);	    /* Write to Gyro Configuration register */
+	wiringPiI2CWriteReg8 (fd, INT_ENABLE, 0x01);	/* Write to interrupt enable register */
 
 	} 
 short read_raw_data(int addr){
