c788000b32384e118c1e8cf870b41bf9225900d1
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-02-12 19:28:24 -0500

    Update client.js

--- socket/client.js
+++ socket/client.js
@@ -1,4 +1,4 @@
-const {io} = require("socket.io");
+const {io} = require("socket.io-client");
 const { spawnSync, execSync } = require("child_process");
 
 const socket = io("http://192.168.0.177:3000");
