81af4c628e2aef8d1c04f0f4d0536871d7b21926
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-02-12 20:12:21 -0500

    Update gyro.cpp

--- driver/gyro.cpp
+++ driver/gyro.cpp
@@ -88,8 +88,8 @@ int main(){
 
 		// printf("\n Gx=%.3f °/s\tGy=%.3f °/s\tGz=%.3f °/s\tAx=%.3f g\tAy=%.3f g\tAz=%.3f g\n",Gx,Gy,Gz,Ax,Ay,Az);
 		// printf("\n Px=%.3f °\tPy=%.3f °\tPz=%.3f °\n\tAx=%.3f \tAy=%.3f g\tAz=%.3f g\n",Px,Py,Pz,Dx,Dy,Dz);
-		printf("\n\tAx=%.3f \tAy=%.3f g\tAz=%.3f g\n",Dx,Dy,Dz);
-
+		printf("%.3f,%.3f,%.3f",Dx,Dy,Dz);
+		system("clear");
 		delay(delay_time);
 	}
 	return 0;
