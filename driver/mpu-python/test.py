0af997be604e21c279c1bb146efd6bf47543b6ec
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-07-17 23:30:24 -0400

    Update test.py

--- driver/mpu-python/test.py
+++ driver/mpu-python/test.py
@@ -3,27 +3,22 @@ from Inertial import InertialSensor
 
 sio = socketio.Client()
 sensor = InertialSensor()
-connected = False
 
 time.sleep(1) # delay necessary to allow mpu9250 to settle
 
 @sio.event
 def disconnect():
-    connected = False
-
     print('disconnected from server')
 
 @sio.event
 def connect():
-    connected = True
     print('connection established')
-
     sio.emit("ID", 'python-gyro-client')
-    # print('recording data')
+
     initLoop()
 
 def initLoop ():
-    while connected:
+    while sio.handle_sigint:
         # try:
         print("EMITING")
         data = sensor.read_data()
